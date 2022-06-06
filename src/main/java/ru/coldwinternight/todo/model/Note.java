e8dd95a8b37a8b5bb853bb9e7188f86ce09a4a44
silendor <hermit256@gmail.com>
2022-06-07 03:55:45 +0400

    refactor Note; remove second toModel()

--- src/main/java/ru/coldwinternight/todo/model/Note.java
+++ src/main/java/ru/coldwinternight/todo/model/Note.java
@@ -10,42 +10,19 @@ import ru.coldwinternight.todo.entity.NoteEntity;
 @AllArgsConstructor
 public class Note {
     private Integer id;
-    private Integer userId;
-    private String userName;
-    private Integer directoryId; // can be null
-    private String directoryName; // can be null
+    private Integer user_id;
+//    private String userName;
+    private Integer directory_id; // can be null
+//    private String directoryName; // can be null
     private String note;
     private String title;
     private boolean completed;
-
-    // default constructor
-    public Note(Integer id, Integer userId, String userName, String note, String title, boolean completed) {
-        this.id = id;
-        this.userId = userId;
-        this.userName = userName;
-        this.note = note;
-        this.title = title;
-        this.completed = completed;
-    }
 
     public static Note toModel(NoteEntity entity) {
         return new Note(
                 entity.getId(),
                 entity.getUser().getId(),
-                entity.getUser().getUsername(),
-                entity.getNote(),
-                entity.getTitle(),
-                entity.isCompleted()
-        );
-    }
-
-    public static Note toModelWithDirectory(NoteEntity entity) {
-        return new Note(
-                entity.getId(),
-                entity.getUser().getId(),
-                entity.getUser().getUsername(),
-                entity.getDirectory().getId(),
-                entity.getDirectory().getName(),
+                entity.getDirectory() == null ? null : entity.getDirectory().getId(),
                 entity.getNote(),
                 entity.getTitle(),
                 entity.isCompleted()
