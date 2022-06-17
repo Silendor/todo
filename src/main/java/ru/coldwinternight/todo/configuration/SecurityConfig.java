e0019f8f9d703198101a6c69fd7ff2013fbd1fa5
silendor <hermit256@gmail.com>
2022-06-17 09:35:31 +0400

    extend WebSecurityConfigurerAdapter; configure HttpSecurity

--- src/main/java/ru/coldwinternight/todo/configuration/SecurityConfig.java
+++ src/main/java/ru/coldwinternight/todo/configuration/SecurityConfig.java
@@ -2,14 +2,26 @@ package ru.coldwinternight.todo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic()
+        .and()
+            .authorizeRequests()
+                .antMatchers("/", "/login/*", "/login").permitAll()
+                .anyRequest().authenticated();
     }
 }
