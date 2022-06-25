435ec971acb0910441644b1d3b76efa314ddc39f
silendor <hermit256@gmail.com>
2022-06-25 04:27:59 +0400

    move password encoder to new config file

--- src/main/java/ru/coldwinternight/todo/TodoApplication.java
+++ src/main/java/ru/coldwinternight/todo/TodoApplication.java
@@ -3,10 +3,6 @@ package ru.coldwinternight.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
 public class TodoApplication extends SpringBootServletInitializer {
@@ -14,4 +10,5 @@ public class TodoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
+
 }
