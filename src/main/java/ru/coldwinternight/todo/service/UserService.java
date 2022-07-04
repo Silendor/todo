9bf26dbb1a90d6b0549f1eb42f67ecfbee98945b
silendor <hermit256@gmail.com>
2022-07-04 19:02:07 +0400

    implement UserDetailsService; add loadUserByUsername with zero authorities and email as username

--- src/main/java/ru/coldwinternight/todo/service/UserService.java
+++ src/main/java/ru/coldwinternight/todo/service/UserService.java
@@ -1,7 +1,10 @@
 package ru.coldwinternight.todo.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,13 +15,15 @@ import ru.coldwinternight.todo.exception.UserNotFoundException;
 import ru.coldwinternight.todo.model.User;
 import ru.coldwinternight.todo.repository.UserRepository;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class UserService implements UserServices {
+public class UserService implements UserServices, UserDetailsService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -50,12 +55,24 @@ public class UserService implements UserServices {
     }
 
     @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // username that user login with = email
+        UserEntity user = userRepository.findByEmailIgnoreCase(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User %s not found", email)));
+
+        // don't have any authorities
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+
+    @Override
     @Transactional
     public void update(UserEntity user, int id) throws UserNotFoundException {
         userRepository.findById(id)
                 .orElseThrow(UserNotFoundException::new);
         if (user.getTasks() != null)
             throw new InvalidDataAccessApiUsageTasksFromUsersApiException();
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setId(id);
         userRepository.save(user);
     }
