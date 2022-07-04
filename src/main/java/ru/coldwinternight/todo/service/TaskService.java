38425be548396ba1ef401b2348be874f5ebfd58c
silendor <hermit256@gmail.com>
2022-07-04 18:58:29 +0400

    remove unnecessary import

--- src/main/java/ru/coldwinternight/todo/service/TaskService.java
+++ src/main/java/ru/coldwinternight/todo/service/TaskService.java
@@ -1,7 +1,6 @@
 package ru.coldwinternight.todo.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.coldwinternight.todo.entity.TaskEntity;
