88b85b416dd77fea07392edb7749ec4d10a197ad
silendor <hermit256@gmail.com>
2022-07-07 17:55:07 +0400

    simplify ExpiresAt with sql.Date; remove access_token Header with Authorization Header; remove comments; inject jwtConfig and algorithm

--- src/main/java/ru/coldwinternight/todo/filter/CustomAuthenticationFilter.java
+++ src/main/java/ru/coldwinternight/todo/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,8 @@ package ru.coldwinternight.todo.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.coldwinternight.todo.configuration.JwtConfig;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -16,14 +19,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 import java.util.Date;
 
+@RequiredArgsConstructor
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
-
-    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
+    private final Algorithm algorithm;
+    private final JwtConfig jwtConfig;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
@@ -41,21 +44,14 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                                             FilterChain chain,
                                             Authentication authentication) throws IOException, ServletException {
         User user = (User) authentication.getPrincipal();
-        String secret = "3QdmqX7zLtkClSbC44b3r53rZwhcqKB9XVskzFmHQKRPlzKZ9RnV9RVTp7xPdNjnHq6N4BDGfc2SDPMTjJkK25SNC9wJHpGH6SG9";
-        Algorithm algorithm = Algorithm.HMAC256(secret.getBytes(StandardCharsets.UTF_8));
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 60 * 1000))
+                .withExpiresAt(java.sql.Date.valueOf(LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays())))
                 .withIssuer(request.getRequestURI())
+                // no roles
 //                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
-//        String refresh_token = JWT.create()
-//                .withSubject(user.getUsername())
-//                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 60 * 1000))
-//                .withIssuer(request.getRequestURI())
-//                .sign(algorithm);
 
-        response.setHeader("access_token", access_token);
-//        response.setHeader("refresh_token", refresh_token);
+        response.addHeader(jwtConfig.getAuthorizationHeader(), jwtConfig.getTokenPrefix() + access_token);
     }
 }
