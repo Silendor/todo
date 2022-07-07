3d400a04a7ecf1bae172176ba3af41b78e88cb40
silendor <hermit256@gmail.com>
2022-07-07 20:29:32 +0400

    remove unused input

--- src/main/java/ru/coldwinternight/todo/configuration/SecurityConfig.java
+++ src/main/java/ru/coldwinternight/todo/configuration/SecurityConfig.java
@@ -2,7 +2,6 @@ package ru.coldwinternight.todo.configuration;
 
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,7 +10,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.coldwinternight.todo.filter.JwtUsernameAndPasswordAuthenticationFilter;
