841857faf91eda42d287974ebd8c7a58bbb5c72a
silendor <hermit256@gmail.com>
2022-07-13 14:23:14 +0400

    create user returns its id in message

--- src/main/java/ru/coldwinternight/todo/controller/UserController.java
+++ src/main/java/ru/coldwinternight/todo/controller/UserController.java
@@ -52,7 +52,7 @@ public class UserController implements UniversalController {
     public ResponseEntity<?> create(@Valid @RequestBody UserEntity user) {
         try {
             UserEntity userEntity = userService.create(user);
-            String successfullyCreatedMessage = String.format("User %d created successfully", userEntity.getId());
+            String successfullyCreatedMessage = String.format("User with id %d created successfully", userEntity.getId());
             return new ResponseEntity<>(successfullyCreatedMessage, HttpStatus.CREATED);
         } catch (UserAlreadyExistException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
