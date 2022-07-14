8b4c007a55c4b62a0c22638d655bc38df823798a
silendor <hermit256@gmail.com>
2022-07-14 14:14:49 +0400

    switch title and taskBody in populateDB.sql

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -4,7 +4,7 @@ INSERT INTO users VALUES
 (2, 'merchant', 'trader@email.old', '$2a$12$zfNUTBsNRLAzJ2XRhFGop.i4e0XuAkQRhMBZWGlzwrdB8S6YrXO3O', 2, false);
 -- goldgoldgold
 INSERT INTO tasks VALUES
-(1, 1, 'test task 1 user1', 'title 1', false, false),
-(2, 1, 'test task 2 user1', 'title 2', false, false),
-(3, 2, 'test task 1 user2', 'my first title', false, false),
-(4, 1, 'test task 3 user1', 'title 3', false, false);
+(1, 1, 'title 1', 'test task 1 user1', false, false),
+(2, 1, 'title 2', 'test task 2 user1', false, false),
+(3, 2, 'my first title', 'test task 1 user2', false, false),
+(4, 1, 'title 3', 'test task 3 user1', false, false);
